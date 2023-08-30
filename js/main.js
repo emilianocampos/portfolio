81080d0e89ce00ef68a585cb30dc7a17a04a2728
emilianocampos <emixion17@gmail.com>
2023-08-30 07:08:45 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+
+
+
+
+
 const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 3400,
+    timer: 5000,
     timerProgressBar: true,
     didOpen: (toast) => {
         toast.addEventListener('mouseenter', Swal.stopTimer)
@@ -16,7 +21,7 @@ Toast.fire({
     imageUrl: 'https://media.tenor.com/B64bnz6gRjEAAAAi/wave-hello.gif',
     width: '200px',
     imageWidth: '70px',
-   color: 'white',
+    color: 'white',
     font: 'bold'
 })
 
@@ -27,8 +32,45 @@ AOS.init();
 //-descargar cv 
 let btn = document.getElementById('downloadButton');
 
-btn.addEventListener('click', () =>{
 
-    window.location.href = './doc/Cv Emiliano Campos.pdf'
-    
+
+btn.addEventListener('click', () => {
+
+
+    Swal.fire({
+        title: 'Estas seguro?',
+        text: "Se descargara un archivo .pdf!",
+        imageUrl: './img/runcarpet.gif',
+        showCancelButton: true,
+        confirmButtonColor: '#1ca334',
+        cancelButtonColor: '#d33',
+
+        confirmButtonText: 'Si, descargar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            window.location.href = './doc/Cv Emiliano Campos.pdf'
+        }
+    })
 })
+
+
+
+function irArriba() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+// Mostrar el botón cuando el usuario desplaza hacia abajo
+window.addEventListener("scroll", function() {
+    const btnIrArriba = document.getElementById("btnIrArriba");
+    if (window.pageYOffset > 100) {
+        btnIrArriba.style.display = "block";
+    } else {
+        btnIrArriba.style.display = "none";
+    }
+});
+
+
